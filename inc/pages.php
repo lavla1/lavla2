d5c27b405fa7832bb5a0a4c8d3ba7e2aaf956d53
Zack P <zack@zacklearns.com>
2017-10-30 14:58:15 -0600

    Create pages.php

---
+++ inc/pages.php
@@ -0,0 +1,8 @@
+<?php
+$pages = {}
+$data = {}
+# Format: pages[<PAGE NAME>] = <VIEW NAME>
+# Format: data[<VIEW NAME>] = "<TABLE>"
+pages["home"] = "index";
+data["index"] = "{'My Website','Welcome to my website!'}"
+?>
